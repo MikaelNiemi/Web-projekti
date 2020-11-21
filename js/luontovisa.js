d869f46bdff6522f0d538a8df98943ba8fee358c
Jyri Ahola <n0ahjy00@students.oamk.fi>
2020-11-21 18:51:52 +0200

    kuvien ja tulosruudun lisaaminen luontovisaan

--- js/luontovisa.js
+++ js/luontovisa.js
@@ -1,5 +1,5 @@
 
-//Lista kysymyksistä ja vastauksista
+//Kysymykset ja vastaukset arrayssa
 const questions = [
     {
         question: "Millä suomen linnuista on maailman pisin muuttomatka?",
@@ -29,19 +29,35 @@ const questions = [
     }
 ]
 
-
+//Lisätiedot oikeasta vastauksesta arrayssa
 const tietoa = [
-    "Pisimmän muuttomatkan maailmassa tekee lapintiira.<br>Lapintiira voi lentää jopa 81 000 kilometrin muuttomatkan.<br>Arvioiden mukaan lapintiira lentää koko elämänsä aikana 2.4 miljoonaa kilometria. ",
+    'Pisimmän muuttomatkan maailmassa tekee lapintiira.<br>Lapintiira voi lentää jopa 81 000 kilometrin muuttomatkan.<br>Arvioiden mukaan lapintiira lentää koko elämänsä aikana 2.4 miljoonaa kilometria.',
 
-    "Suomen suurin nisäkäs on hirvi. <br>Aikuinen hirvi voi kasvaa jopa 800kg painoiseksi ja kolme metriä pitkäksi. ",
+    'Suomen suurin nisäkäs on hirvi. <br>Aikuinen hirvi voi kasvaa jopa 800kg painoiseksi ja kolme metriä pitkäksi. ',
 
-    "Suomen yleisin kalalaji on ahven. Ahvenen levinneisyys kattaa lähes koko suomen.<br>Ainoastaan pohjoinen käsivarsi on paikka jossa ahventa ei tavata. ",
+    'Suomen yleisin kalalaji on ahven. Ahvenen levinneisyys kattaa lähes koko suomen.<br>Ainoastaan pohjoinen käsivarsi on paikka jossa ahventa ei tavata. ',
 
-    "Suomen pienin lintulaji on hippiäinen. Hippiäisen koko on 8,5cm - 9,5cm ja paino 4,5-7g. <br> Hippiäinen on myös euroopan pienin lintu. ",
+    'Suomen pienin lintulaji on hippiäinen. Hippiäisen koko on 8,5cm - 9,5cm ja paino 4,5-7g. <br> Hippiäinen on myös euroopan pienin lintu. ',
 
-    "Suomen yleisin lintulaji on pajulintu. <br>Pajulintu on levinnyt koko maan alueelle ja suomessa pesii nykyisin 7 - 11 miljoonaa paria. "
+    'Suomen yleisin lintulaji on pajulintu. <br>Pajulintu on levinnyt koko maan alueelle ja suomessa pesii nykyisin 7 - 11 miljoonaa paria. '
 
 ]
+
+const kuvat = [
+    '<img src="img/lapintiira-web.jpg" class="img-fluid" alt=""/>',
+    '<img src="img/hirvi-web.jpg" class="img-fluid" alt=""/>',
+    '<img src="img/ahven-web.jpg" class="img-fluid" alt=""/>',
+    '<img src="img/hippiainen-web.jpg" class="img-fluid" alt=""/>',
+    '<img src="img/pajulintu-web.jpg" class="img-fluid" alt=""/>'
+]
+
+
+const palkinnot = [
+    '<img src="img/trophy-web.png" class="img-fluid" alt=""/>',
+    '<img src="img/thumbsup-web.png" class="img-fluid" alt=""/>',
+    '<img src="img/thumbsdown-web.png" class="img-fluid" alt=""/>',
+]
+
 
 let vastattu = 0
 let oikein = 0
@@ -58,6 +74,8 @@ function setQuestion() {
     $("#question").removeClass("hide")
     $("#question").html(questions[random].question)
     $("#vastaus").html(tietoa[random])
+    $("#kuva").html(kuvat[random])
+
     for (let i = 0; i < 4; i++) {
         //Poistetaan vanhat luokat vastauksista ensin
         $(`#answer-${i}`).removeClass("correct")
@@ -71,8 +89,9 @@ function setQuestion() {
         }
     }
     questions.splice(random, 1)
-    tietoa.splice(random,1)
-}
+    tietoa.splice(random, 1)
+    kuvat.splice(random, 1)
+};
 
 function resetProps() {
     $(".correct").prop("disabled", false)
@@ -82,7 +101,7 @@ function resetProps() {
     $("#vastaus").addClass("hide")
     $("#kuva").addClass("hide")
     $("#next").addClass("hide")
-}
+};
 
 
 $(document).ready(function () {
@@ -93,13 +112,12 @@ $(document).ready(function () {
 
     //Funktio vastauksille
     $("#answers button").click(function () {
-        
-        //$(".correct").css("background-color", "#19ba57")
-        //$(".false").css("background-color", "#bd3b36")
         $(".correct").prop("disabled", true)
         $(".false").prop("disabled", true)
         $("#vastaus").removeClass("hide")
         $("#next").removeClass("hide")
+        $("#kuva").removeClass("hide")
+
         if ($(this).hasClass("correct")) {
             $("#correct").removeClass("hide")
             $(this).css("background-color", "#19ba57")
@@ -111,16 +129,26 @@ $(document).ready(function () {
     })
 
     $("#next").click(function () {
-        if(vastattu === 4){
+        if (vastattu === 4) {
             $("#answers").addClass("hide")
             $("#question").addClass("hide")
             $("#vastaus").addClass("hide")
             $("#next").addClass("hide")
             $("#reset").removeClass("hide")
+            $("#kuva").addClass("hide")
+            $("#prize").removeClass("hide")
             $("#results").append(`<h1>Sait ${oikein}/5 oikein!</h1>`)
-
-        }else{
-        
+            if(oikein === 5){
+                $("#prize").append(palkinnot[0])
+                $("#results").append("Hienoa työtä! Olet oikea luontomestari!")
+            }else if(oikein < 5 && oikein > 2){
+                $("#prize").append(palkinnot[1])
+                $("#results").append("Melkein kaikki oikein! Yritä vielä uudestaan jos saisit ensi kerralla täydet pisteet!")
+            }else{
+                $("#prize").append(palkinnot[2])
+                $("#results").append("Vastauksissa on vielä parantamisen varaa. Yritä uudelleen jos seuraava kerta menisi jo paremmin!")
+            }
+        } else {
             vastattu++
             console.log(vastattu)
             resetProps()
