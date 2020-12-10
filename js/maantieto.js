0991eaeee9fd5794b829e54971035c939df7eaed
Enni Dahlström <n0daen00@students.oamk.fi>
2020-12-10 11:11:41 +0200

    Maantietovisoja päivitetty ja tietoja-sivua siistitty

--- js/maantieto.js
+++ js/maantieto.js
@@ -1,5 +1,12 @@
  $(document).ready(function() {
 
+   // Kun vaihtoehto valitaan, 
+   //   - lukitaan muut kohdan vastausvaihtoehdot, valintaa ei voi muuttaa
+   //   - tarkistetaan heti onko vastaus oikein vai ei,
+   //   - merkitään vastaus joko vihreäksi tai punaiseksi,
+   //   - tulostetaan vastaava palaute
+   
+   // KYSYMYS 1
    $("[name=kysymys1]").click(function(){
      let vastaus = $("input[name=kysymys1]:checked").val();
       if (vastaus === "oikein") {
@@ -20,86 +27,89 @@
       });
    });
 
-      $("[name=kysymys2]").click(function(){
-         let vastaus = $("input[name=kysymys2]:checked").val();
-          if (vastaus === "oikein") {
-             $(this).parent().addClass("vihreä");
-             $("#palaute2").html("<b>Kyllä!</b> Vuodenaikojen vaihtelu on seurausta sekä maan akselin kallistumisesta että <b>maan kiertoradasta Auringon  ympäri</b>.");
-             $("#palaute2").removeClass("invisible"); // (<a href='https://fi.wikipedia.org/wiki/Vuodenaika' target='_blank'>LÄHDE</a>)
-           } else {
-             $(this).parent().addClass("punainen");
-             $("#palaute2").html("Ei aivan. Vuodenaikojen vaihtelu johtuu siitä, että <b>maa kiertää aurinkoa</b>.");
-             $("#palaute2").removeClass("invisible");
-          }  
-       
-          let vastaukset = $(this).parent().parent().children();
-          vastaukset.each(function(){
-             let valintanappi = $(this).children().first();
-             valintanappi.prop("disabled", true); 
-           
-          });
-         });
-
-          $("[name=kysymys3]").click(function(){
-            let vastaus = $("input[name=kysymys3]:checked").val();
-             if (vastaus === "oikein") {
-                $(this).parent().addClass("vihreä");
-                $("#palaute3").html("<b>Oikein!</b> Luode sijaitsee pohjoisen ja lännen välissä. (Englanniksi luodetta kutsutaankin nimellä North-West, eli Pohjoinen-Länsi.)");
-                $("#palaute3").removeClass("invisible");
-              } else {
-                $(this).parent().addClass("punainen");
-                $("#palaute3").html("Väärin meni. <b>Luode</b> on oikea vastaus!");
-                $("#palaute3").removeClass("invisible");
-             }  
-          
-             let vastaukset = $(this).parent().parent().children();
-             vastaukset.each(function(){
-                let valintanappi = $(this).children().first();
-                valintanappi.prop("disabled", true); 
+      // KYSYMYS 2
+   $("[name=kysymys2]").click(function(){
+      let vastaus = $("input[name=kysymys2]:checked").val();
+         if (vastaus === "oikein") {
+            $(this).parent().addClass("vihreä");
+            $("#palaute2").html("<b>Kyllä!</b> Vuodenaikojen vaihtelu on seurausta sekä maan akselin kallistumisesta että <b>maan kiertoradasta Auringon  ympäri</b>.");
+            $("#palaute2").removeClass("invisible");
+         } else {
+            $(this).parent().addClass("punainen");
+            $("#palaute2").html("Ei aivan. Vuodenaikojen vaihtelu johtuu siitä, että <b>maa kiertää aurinkoa</b>.");
+            $("#palaute2").removeClass("invisible");
+         }  
+      
+         let vastaukset = $(this).parent().parent().children();
+         vastaukset.each(function(){
+            let valintanappi = $(this).children().first();
+            valintanappi.prop("disabled", true); 
          
-             });
-            });
-
-             $("[name=kysymys4]").click(function(){
-               let vastaus = $("input[name=kysymys4]:checked").val();
-                if (vastaus === "oikein") {
-                   $(this).parent().addClass("vihreä");
-                   $("#palaute4").html("<b>Oikein!</b> Välimeri ei ole valtameri.");
-                   $("#palaute4").removeClass("invisible");
-                 } else {
-                   $(this).parent().addClass("punainen");
-                   $("#palaute4").html("Ei! Näistä vaihtoehdoista kaikki <b>Välimereä</b> lukuunottamatta ovat valtameriä.");
-                   $("#palaute4").removeClass("invisible");
-                }  
-             
-                let vastaukset = $(this).parent().parent().children();
-                vastaukset.each(function(){
-                   let valintanappi = $(this).children().first();
-                   valintanappi.prop("disabled", true); 
-            
-                });
-               });
-
-                $("[name=kysymys5]").click(function(){
-                  let vastaus = $("input[name=kysymys5]:checked").val();
-                   if (vastaus === "oikein") {
-                      $(this).parent().addClass("vihreä");
-                      $("#palaute5").html("<b>Kyllä!</b> Kääntöpiiri on leveyspiiri 23,4 astetta sekä pohjoisella että eteläisellä pallonpuoliskolla.<br>Pohjoisessa sijaitsee Kravun kääntöpiiri, etelässä taas Kauriin kääntöpiiri.");
-                      $("#palaute5").removeClass("invisible"); // (<a href='https://fi.wikipedia.org/wiki/K%C3%A4%C3%A4nt%C3%B6piiri' target='_blank'>LÄHDE</a>)
-                     
-                    } else {
-                      $(this).parent().addClass("punainen");
-                      $("#palaute5").html("Oho, pieleen meni.<br><br>Kääntöpiiri on leveyspiiri 23,4 astetta sekä pohjoisella että eteläisellä pallonpuoliskolla.<b>Pohjoisessa sijaitsee Kravun kääntöpiiri</b>, etelässä taas Kauriin kääntöpiiri.");
-                      $("#palaute5").removeClass("invisible"); //(<a href='https://fi.wikipedia.org/wiki/K%C3%A4%C3%A4nt%C3%B6piiri' target='_blank'>LÄHDE</a>)
-                   }  
-                
-                   let vastaukset = $(this).parent().parent().children();
-                   vastaukset.each(function(){
-                      let valintanappi = $(this).children().first();
-                      valintanappi.prop("disabled", true); 
-               
-                   });
+         });
    });
+
+   // KYSYMYS 3
+      $("[name=kysymys3]").click(function(){
+      let vastaus = $("input[name=kysymys3]:checked").val();
+         if (vastaus === "oikein") {
+            $(this).parent().addClass("vihreä");
+            $("#palaute3").html("<b>Oikein!</b> Luode sijaitsee pohjoisen ja lännen välissä. (Englanniksi luodetta kutsutaankin nimellä North-West, eli Pohjoinen-Länsi.)");
+            $("#palaute3").removeClass("invisible");
+         } else {
+            $(this).parent().addClass("punainen");
+            $("#palaute3").html("Väärin meni. <b>Luode</b> on oikea vastaus!");
+            $("#palaute3").removeClass("invisible");
+         }  
+      
+         let vastaukset = $(this).parent().parent().children();
+         vastaukset.each(function(){
+            let valintanappi = $(this).children().first();
+            valintanappi.prop("disabled", true); 
+   
+         });
+      });
+
+   // KYSYMYS 4
+      $("[name=kysymys4]").click(function(){
+      let vastaus = $("input[name=kysymys4]:checked").val();
+         if (vastaus === "oikein") {
+            $(this).parent().addClass("vihreä");
+            $("#palaute4").html("<b>Oikein!</b> Välimeri ei ole valtameri.");
+            $("#palaute4").removeClass("invisible");
+         } else {
+            $(this).parent().addClass("punainen");
+            $("#palaute4").html("Ei! Näistä vaihtoehdoista kaikki <b>Välimereä</b> lukuunottamatta ovat valtameriä.");
+            $("#palaute4").removeClass("invisible");
+         }  
+      
+         let vastaukset = $(this).parent().parent().children();
+         vastaukset.each(function(){
+            let valintanappi = $(this).children().first();
+            valintanappi.prop("disabled", true); 
+   
+         });
+      });
+
+   // KYSYMYS 5
+      $("[name=kysymys5]").click(function(){
+      let vastaus = $("input[name=kysymys5]:checked").val();
+         if (vastaus === "oikein") {
+            $(this).parent().addClass("vihreä");
+            $("#palaute5").html("<b>Kyllä!</b> Kääntöpiiri on leveyspiiri 23,4 astetta sekä pohjoisella että eteläisellä pallonpuoliskolla.<br>Pohjoisessa sijaitsee Kravun kääntöpiiri, etelässä taas Kauriin kääntöpiiri.");
+            $("#palaute5").removeClass("invisible"); 
+         } else {
+            $(this).parent().addClass("punainen");
+            $("#palaute5").html("Oho, pieleen meni.<br><br>Kääntöpiiri on leveyspiiri 23,4 astetta sekä pohjoisella että eteläisellä pallonpuoliskolla.<b>Pohjoisessa sijaitsee Kravun kääntöpiiri</b>, etelässä taas Kauriin kääntöpiiri.");
+            $("#palaute5").removeClass("invisible");
+         }  
+      
+         let vastaukset = $(this).parent().parent().children();
+         vastaukset.each(function(){
+            let valintanappi = $(this).children().first();
+            valintanappi.prop("disabled", true); 
+   
+         });
+});
 
 
    $("#tarkista").click(function(){
@@ -107,12 +117,12 @@
       $("#modal-title").html("");
       $("#modal-body").html("");
 
-      // Lasketaan oikeat ja väärät vastaukset
+      // Lasketaan oikeat/väärät vastaukset, sekä mahd. tyhjät kohdat
       let oikeat = [];
       let väärät = [];
       let vastaamattomat = []; 
          
-      // Lasketaan oikeat/väärät vastaukset, sekä mahd. tyhjät kohdat
+      
          let vastaus1 = $("input[name=kysymys1]:checked").val();
           if (vastaus1 === "oikein") {
              oikeat.push(vastaus1);
@@ -158,15 +168,14 @@
              vastaamattomat.push(vastaus5);
           }
           
-            
-// Arvosanan tulostus
+// Virheilmoitus, jos johonkin ei ole vastattu
          if (vastaamattomat.length > 0) {
             $("#modal-title").html("Virhe:");
             $("#modal-body").html("Sinun tulee ensin vastata kaikkiin kohtiin!");
             $("#ilmoitus").modal("show");
             return;
          } else {
-            }
+            } // Tulostetaan 5 tähteä vastausten perusteella
             for (let j = 0; j < oikeat.length; j++) {
                $("#modal-body").append('<i class="fa fa-star" aria-hidden="true"></i>');
             } 
--- js/maantieto2.js
+++ js/maantieto2.js
@@ -2,14 +2,21 @@ $(document).ready(function(){
     const tulos = "<b>Sait yhteensä <span class='tulos'></span>/5 oikein!</b>"
     const kysymykset = ["#kysymys1", "#kysymys2", "#kysymys3", "#kysymys4", "#kysymys5"];
     const oikeat = ["Riika", "Euro", "Etelä-Amerikka", "Tanska", "Venäjä"];
+    const selitys = ["Latvian pääkaupunki Riika perustettiin vuonna 1201. Väkiluku on noin 630 000.",
+                    "Ennen euroa Espanjassa käytettiin pesetoja. Euroon siirryttiin 1.1.2002.",
+                    "Argentiina sijaitsee Etelä-Amerikassa, Atlantin länsirannikolla. Sen pääkaupunki on Buenos Aires.",
+                    "Tanskan rajanaapureita on vain yksi; Saksa. Suomen rajanaapureihin sen sijaan kuuluu Ruotsi, Norja ja Venäjä.",
+                    "Venäjän pinta-ala on huimat 17 100 000 neliökilometriä. Toiseksi suurin on Kanada (9 990 000 neliökilometriä)"];
     let vastaukset = [];
     let esillä = 0;
     let pisteet = 0;
 
+    // Seuraava-painike aktivoituu, kun valitaan yksi vaihtoehdoista
     $("[type=radio]").click(function(){
         $("#seuraava").prop("disabled", false);
     });
 
+    // Kun seuraava-painiketta klikataan, nykyinen kysymys sulkeutuu; avautuu seuraava kysymys
     $("#seuraava").click(function(){
         $("#seuraava").prop("disabled",true);
         let vastaus = $(kysymykset[esillä]).children(".radio-inline").children(":checked").parent("label").contents().text();
@@ -27,6 +34,8 @@ $(document).ready(function(){
         }
     });
 
+    // Kun viimeiseen kysymykseen on vastattu
+    // Lopeta visa-painike: 
     $("#lopeta_visa").click(function(){
         $("#lopeta_visa").addClass("piilossa");
         $("#lopputulos").removeClass("piilossa");
@@ -35,19 +44,29 @@ $(document).ready(function(){
         $("#lopputulos").html(tulos);
         $(".tulos").html(pisteet);
     
+        // Tarkistetaan onko vastaus oikein, 
+        //      jos on, tulostetaan check-symboli
         for (let i = 0; i < kysymykset.length; i++) {
             let kysymys_nro = i + 1;
             if (vastaukset[i] === oikeat[i]) {
                $("#oma" + i).html(kysymys_nro + ". " + "<i>" + vastaukset[i] + "</i>"
                     + '<i class="fa fa-check" aria-hidden="true"></i>'); 
                $("#oikea" + i).html("-");
-            } else {
+        //      jos ei, tulostetaan rastisymboli ja kerrotaan oikea vastaus
+            } else { 
                 $("#oma" + i).html(kysymys_nro + ". " + "<i>" + vastaukset[i] + "</i>"
                     + ' <i class="fa fa-times" aria-hidden="true"></i>');
                 $("#oikea" + i).html("<b>Oikea vastaus:</b> " + oikeat[i]);
             }
+
+        $("#oikea" + i).prepend('<a class="selitys" data-container="body" data-toggle="popover" data-placement="right" data-content="' 
+            + selitys[i] + '" data-trigger="focus" tabindex="-1"><i class="fa fa-info-circle" aria-hidden="true"></i></a>   ');
         }
+
+        // Kysymysmerkkien popoverit 
+        $("[data-toggle=popover]").popover();
        
+        // Kuvan tulostus pisteiden määrän perusteella
         if (pisteet === 5) {
             $("#palkinto").html('<img src="img/gold-trophy.png" alt="kultapokaali"></img>');
         } else if (pisteet < 5 && pisteet > 2 ) {
